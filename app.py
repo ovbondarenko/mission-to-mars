7203d4af09055670b35d999f4d420cc0f0942194
ovbondarenko <ob@pienergy.com>
2019-04-15 17:16:17 -0700

    Add routes to flask

--- app.py
+++ app.py
@@ -1,47 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 # Import our pymongo library, which lets us connect our Flask app to our Mongo database.
 import pymongo
+import mission_to_mars
+from flask_pymongo import PyMongo
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
 # Create connection variable
-conn = 'mongodb://localhost:27017'
-
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
-
-# Connect to a database. Will create one if not already available.
-db = client.team_db
-
-# Drops collection if available to remove duplicates
-db.team.drop()
-
-# Creates a collection in the database and inserts two documents
-db.team.insert_many(
-    [
-        {
-            'player': 'Jessica',
-            'position': 'Point Guard'
-        },
-        {
-            'player': 'Mark',
-            'position': 'Center'
-        }
-    ]
-)
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 # Set route
 @app.route('/')
-def indx():
+def index():
     # Store the entire team collection in a list
-    teams = list(db.team.find())
-    print(teams)
+    news_instance = mongo.db.request_instances.find_one()
+    return render_template('index.html', news = news_instance)
 
-    # Return the template with the teams list passed in
-    return render_template('index.html', teams=teams)
+
+@app.route("/scrape")
+def scraper():
+    news = mongo.db.request_instances
+    news_update = mission_to_mars.scrape()
+    news.update({}, news_update, upsert=True)
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- mission_to_mars.py
+++ mission_to_mars.py
@@ -1,205 +1,128 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[7]:
-
-
 from splinter import Browser
 from bs4 import BeautifulSoup
 from pprint import pprint
 import pandas as pd
 import pymongo
-
-
-# In[8]:
-
-
-executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=False)
-
-
-# In[9]:
-
-
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
-
-
-# In[10]:
-
-
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-news_titles = soup.find_all('div', class_ = "content_title")
-news_p = soup.find_all('div', class_ = "article_teaser_body" )
-
-mars_new_instance = {'news-title': news_titles[0].text, 'news-summary': news_p[0].text}
-
-
-# In[11]:
-
-
-img_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(img_url)
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-featured_image_url = soup.find('a', class_= "button fancybox")['data-fancybox-href']
-
-
-# In[12]:
-
-
-featured_image_url = 'https://www.jpl.nasa.gov'+ featured_image_url
-mars_new_instance['featured-image-url'] = featured_image_url
-
-
-# In[13]:
-
-
-tw_url = 'https://twitter.com/marswxreport?lang=en'
-browser.visit(tw_url)
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[14]:
-
-
-latest_tweet = soup.find_all('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
-mars_new_instance['current-weather'] = latest_tweet[0].text
-
-
-# In[15]:
-
-
-sf_url = 'https://space-facts.com/mars/'
-browser.visit(sf_url)
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[16]:
-
-
-mars_attr = soup.find_all('td', class_="column-1")
-mars_facts = soup.find_all('td', class_="column-2")
-for attr, fact in zip(mars_attr, mars_facts):
-    print(attr.text, fact.text)
-
-
-# In[17]:
-
-
-mars_facts_dict = {}
-for attr, fact in zip(mars_attr, mars_facts):
-    mars_facts_dict[attr.text.strip().split(':')[0]] = fact.text.strip()
-
-mars_facts_dict
-
-
-# In[18]:
-
-
-facts_df = pd.DataFrame.from_dict(mars_facts_dict, orient = "index")
-facts_df
-
-
-# In[19]:
-
-
-facts_df.to_html(header = False)
-
-
-# In[20]:
-
-
-hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-browser.visit(hemi_url)
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[21]:
-
-
-hemi_links = soup.find_all('a', class_= "itemLink product-item")
-links = []
-for a in hemi_links:
-    hemi_url = 'https://astrogeology.usgs.gov'+a['href']
-    if hemi_url not in links:
-        links.append(hemi_url)
-print(links)
-
-
-# In[22]:
-
-
 import time
-hemisphere_image_urls = []
-for link in links:
-    browser.visit(link)
-    html = browser.html
-    soup = BeautifulSoup(html, 'html.parser')
-    img_dict = {}
-    img_dict['title'] = soup.find('h2', class_ = "title").text.strip('Enhanced')
-    img_dict['img_url'] = soup.find('a', target = "_blank")['href']
-    hemisphere_image_urls.append(img_dict)
-    time.sleep(3)
 
 
-# In[23]:
+def init_browser():
+        executable_path = {'executable_path': 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'}
+        browser = Browser('chrome', **executable_path, headless=False)
+        return browser
+
+def visit_url(browser, url):
+        browser.visit(url)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup
 
 
-hemisphere_image_urls
+def scrape():
+        browser = init_browser()
+
+        # scrape latest new article
+        soup = visit_url(browser, 'https://mars.nasa.gov/news/')
+
+        news_titles = soup.find_all('div', class_ = "content_title")
+        news_p = soup.find_all('div', class_ = "article_teaser_body")
+
+        print(news_titles, news_p)
+        mars_new_instance = {'news-title': news_titles[0].a.text, 
+                                'news-summary': news_p[0].text}
 
 
-# In[24]:
+        # scrape a featured image from NASA website
+        soup = visit_url(browser, 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
+
+        featured_image_url = soup.find('a', class_= "button fancybox")['data-fancybox-href']
 
 
-# Create connection variable
-conn = 'mongodb://localhost:27017'
+        featured_image_url = 'https://www.jpl.nasa.gov'+ featured_image_url
+        mars_new_instance['featured-image-url'] = featured_image_url
 
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
+        # Scrape latest weather tweet
+        soup = visit_url(browser, 'https://twitter.com/marswxreport?lang=en')
+        latest_tweet = soup.find_all('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
+        
+        # Build a dictionary with latest news, a featured image and latest weather tweet
+        mars_new_instance['current-weather'] = latest_tweet[0].text
 
-# Connect to a database. Will create one if not already available.
-db = client.mars_db
+        return mars_new_instance
 
-
-# In[28]:
-
-
-# Drops collection if available to remove duplicates
-db.request_instances.drop()
-
-# Creates a collection in the database and inserts two documents
-db.request_instances.insert_one(mars_new_instance)
+ 
 
 
-# In[30]:
+def scrape_mars_facts():
+        browser = init_browser()
+
+        sf_url = 'https://space-facts.com/mars/'
+        browser.visit(sf_url)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+
+        mars_attr = soup.find_all('td', class_="column-1")
+        mars_facts = soup.find_all('td', class_="column-2")
+
+        mars_facts_dict = {}
+        for attr, fact in zip(mars_attr, mars_facts):
+                mars_facts_dict[attr.text.strip().split(':')[0]] = fact.text.strip()
 
 
-# Drops collection if available to remove duplicates
-db.facts.drop()
+        facts_df = pd.DataFrame.from_dict(mars_facts_dict, orient = "index")
 
-# Creates a collection in the database and inserts two documents
-db.facts.insert_one(mars_facts_dict)
+        facts_table = facts_df.to_html(header = False)
 
+        return facts_table.replace('\n', '')
 
-# In[31]:
+def scrape_hemisphere_img():
+        browser = init_browser()
 
-
-# Drops collection if available to remove duplicates
-db.images.drop()
-
-# Creates a collection in the database and inserts two documents
-db.images.insert_many(hemisphere_image_urls)
+        hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        browser.visit(hemi_url)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
 
 
-# In[ ]:
+        hemi_links = soup.find_all('a', class_= "itemLink product-item")
+        links = []
+        for a in hemi_links:
+                hemi_url = 'https://astrogeology.usgs.gov'+a['href']
+        if hemi_url not in links:
+                links.append(hemi_url)
 
 
+        hemisphere_image_urls = []
+        for link in links:
+                browser.visit(link)
+                html = browser.html
+                soup = BeautifulSoup(html, 'html.parser')
+                img_dict = {}
+                img_dict['title'] = soup.find('h2', class_ = "title").text.strip('Enhanced')
+                img_dict['img_url'] = soup.find('a', target = "_blank")['href']
+                hemisphere_image_urls.append(img_dict)
+                time.sleep(3)
+
+        return hemisphere_image_urls
 
 
+def write_to_mongo(mars_new_instance):
+
+        conn = 'mongodb://localhost:27017'
+        client = pymongo.MongoClient(conn)
+
+        db = client.mars_db
+
+        db.request_instances.drop()
+        db.request_instances.insert_one(mars_new_instance)
+
+        # db.facts.drop()
+        # db.facts.insert_one(mars_facts_dict)
+
+        # db.images.drop()
+        # db.images.insert_many(hemisphere_image_urls)
+
+mars_news = scrape()
+write_to_mongo(mars_news)
