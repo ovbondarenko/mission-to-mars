7c144c3da4b1c73c1689fea1e283c9395cb68e33
Olesya Bondarenko <ovbondarenko@gmail.com>
2019-04-20 21:27:44 -0700

    Complete flask app. Fix css stylesheet link

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -19,49 +19,31 @@ def visit_url(browser, url):
         soup = BeautifulSoup(html, 'html.parser')
         return soup
 
-
-def scrape():
-        browser = init_browser()
-
+def scrape_news(browser, url):
         # scrape latest new article
-        soup = visit_url(browser, 'https://mars.nasa.gov/news/')
+        soup = visit_url(browser, url)
 
         news_titles = soup.find_all('div', class_ = "content_title")
         news_p = soup.find_all('div', class_ = "article_teaser_body")
+        return news_titles[0].a.text, news_p[0].text
 
-        print(news_titles, news_p)
-        mars_new_instance = {'news-title': news_titles[0].a.text, 
-                                'news-summary': news_p[0].text}
-
-
+def scrape_featured_image(browser, url):
         # scrape a featured image from NASA website
-        soup = visit_url(browser, 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
-
+        soup = visit_url(browser, url)
         featured_image_url = soup.find('a', class_= "button fancybox")['data-fancybox-href']
-
-
         featured_image_url = 'https://www.jpl.nasa.gov'+ featured_image_url
-        mars_new_instance['featured-image-url'] = featured_image_url
+        return featured_image_url
 
+def scrape_weather_tweet(browser, url):
         # Scrape latest weather tweet
-        soup = visit_url(browser, 'https://twitter.com/marswxreport?lang=en')
+        soup = visit_url(browser, url)
         latest_tweet = soup.find_all('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
-        
-        # Build a dictionary with latest news, a featured image and latest weather tweet
-        mars_new_instance['current-weather'] = latest_tweet[0].text
+        return latest_tweet[0].text
 
-        return mars_new_instance
+def scrape_mars_facts(browser, url):
+        # Scrape a table with marse facts and return it as an html <table> string
 
- 
-
-
-def scrape_mars_facts():
-        browser = init_browser()
-
-        sf_url = 'https://space-facts.com/mars/'
-        browser.visit(sf_url)
-        html = browser.html
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = visit_url(browser, url)
 
         mars_attr = soup.find_all('td', class_="column-1")
         mars_facts = soup.find_all('td', class_="column-2")
@@ -77,24 +59,22 @@ def scrape_mars_facts():
 
         return facts_table.replace('\n', '')
 
-def scrape_hemisphere_img():
-        browser = init_browser()
 
-        hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-        browser.visit(hemi_url)
-        html = browser.html
-        soup = BeautifulSoup(html, 'html.parser')
+def scrape_hemisphere_img(browser, url):
+
+        soup = visit_url(browser, url)
 
 
         hemi_links = soup.find_all('a', class_= "itemLink product-item")
         links = []
         for a in hemi_links:
                 hemi_url = 'https://astrogeology.usgs.gov'+a['href']
-        if hemi_url not in links:
-                links.append(hemi_url)
+                if hemi_url not in links:
+                        links.append(hemi_url)
 
 
         hemisphere_image_urls = []
+
         for link in links:
                 browser.visit(link)
                 html = browser.html
@@ -108,6 +88,47 @@ def scrape_hemisphere_img():
         return hemisphere_image_urls
 
 
+def scrape():
+        browser = init_browser()
+
+        mars_new_instance = {}
+
+        try:
+                # scrape latest new article
+                title, news = scrape_news(browser, 'https://mars.nasa.gov/news/')
+
+                # scrape a featured image from NASA website
+                image_url = scrape_featured_image(browser, 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
+
+                # Scrape latest weather tweet
+                latest_tweet = scrape_weather_tweet(browser, 'https://twitter.com/marswxreport?lang=en')
+
+                # Scrape Mars facts:
+                mars_facts = scrape_mars_facts(browser, 'https://space-facts.com/mars/')
+
+                # Scrape all Mars hemisphere images and names
+                hemisphere_image_urls = scrape_hemisphere_img(browser, 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars')
+ 
+
+
+                mars_new_instance = {'news-title': title, 
+                                        'news-summary': news,
+                                        'featured-image-url': image_url,
+                                        'current-weather':latest_tweet,
+                                        'mars-facts': mars_facts,
+                                        'hemisphere-images': hemisphere_image_urls
+                                        }
+
+                write_to_mongo(mars_new_instance)
+
+        except:
+                pass
+
+
+        return mars_new_instance
+
+
+
 def write_to_mongo(mars_new_instance):
 
         conn = 'mongodb://localhost:27017'
@@ -118,11 +139,8 @@ def write_to_mongo(mars_new_instance):
         db.request_instances.drop()
         db.request_instances.insert_one(mars_new_instance)
 
-        # db.facts.drop()
-        # db.facts.insert_one(mars_facts_dict)
 
-        # db.images.drop()
-        # db.images.insert_many(hemisphere_image_urls)
+# mars_news = scrape()
+# print(mars_news)
 
-mars_news = scrape()
-write_to_mongo(mars_news)
+# # 
